90c8431c2e1f4e0f163130ce46580334d8de00fa
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-05-17 10:46:31 +0000

    Footer final

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -3,29 +3,26 @@
 export const Navbar = () => {
   // 3.- Codigo de JS
 
-  const nav = 'Spain 108';
-
-  // 4.- Retorno un solo elemento HTML 
   return (
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
       <div className="container d-flex">
-        <span class="navbar-text text-white">
+        <span className="navbar-text text-white">
           Start Bootstrap
         </span>
-        <div class="justify-content-end">
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div className="justify-content-end">
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
               </li>
             </ul>
           </div>
